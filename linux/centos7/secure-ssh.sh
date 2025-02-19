7f649b4dcc54f0ef617c35297fd0011bfc34b02d
sszalay <scott.szalay@mymail.champlain.edu>
2025-02-19 14:16:10 -0500

    added secure-ssh.sh

---
+++ linux/centos7/secure-ssh.sh
@@ -0,0 +1,6 @@
+#secure-ssh.sh
+#author scott
+#creates a new ssh user using $1 parameter
+#adds a public key from the local repo or curled from the remote repo
+#removes roots ability to ssh in
+echo "ALL YOUR CODE HERE"
